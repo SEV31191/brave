dbfc886facb6b4a27f1e63579bf81fc20cf7919e
Slivca Eugeniu <jekadj549@gmail.com>
2024-02-05 18:11:08 +0200

    added first task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+int number = 453;
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+int result = firstDigit * 10 + thirdDigit;
+Console.WriteLine($"В числе {number} => {result}");
